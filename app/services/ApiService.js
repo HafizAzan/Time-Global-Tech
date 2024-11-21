7bbca66d937abe24ba73f1246063117002ea2548
Hafiz <azan09068@gmail.com>
2024-11-20 23:29:01 -0800

    next js todo list

--- app/services/ApiService.js
+++ app/services/ApiService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://jsonplaceholder.typicode.com";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
